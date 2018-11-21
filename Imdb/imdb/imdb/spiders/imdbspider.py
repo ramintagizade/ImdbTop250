d8ff32078097dffec0eaca717c7e2d0f7eb16a88
Ramin Taghizada <ramintagizade@mail.ru>
2018-11-21 15:15:04 +0300

    refactoring

--- Imdb/imdb/imdb/spiders/imdbspider.py
+++ Imdb/imdb/imdb/spiders/imdbspider.py
@@ -27,35 +27,31 @@ class ImdbSpider(scrapy.Spider) :
         yield scrapy.Request(self.start_urls, self.parse)
 
     def parse(self,response) :
-        self.movie_entity(response)
+        self.get_movie_entity(response)
         if response.xpath(".//tbody[@class='lister-list']/tr/td[@class='titleColumn']/a/@href").extract() :
             urls = response.xpath(".//tbody[@class='lister-list']/tr/td[@class='titleColumn']/a/@href").extract()
         
             for url in urls: 
                 self.ids = self.ids  + 1
-                #if (self.ids == 50):
-                #    break
                 yield scrapy.Request("https://www.imdb.com" + url, callback=self.parse_details,dont_filter=True,meta={'itemId': self.ids-1})
 
-        self.ids = 0   
         yield self.imdb
 
     def parse_details(self,response) :
-        
+
         itemId = response.meta["itemId"]
         duration = response.xpath(".//div[@class='title-overview']/div/div[@class='vital']/div[@class='title_block']/div/div[@class='titleBar']/div[@class='title_wrapper']/div[@class='subtext']/time/text()").extract()
         duration = [x.strip() for x in duration]
         genreDateCountryArray = response.xpath(".//div[@class='title-overview']/div/div[@class='vital']/div[@class='title_block']/div/div[@class='titleBar']/div[@class='title_wrapper']/div[@class='subtext']/a/text()").extract()
-        l = len(genreDateCountryArray)
-        genre = ",".join([genreDateCountryArray[x].strip() for x in range(l-1)])
-        issue_date = genreDateCountryArray[l-1].split("(")[0]
-        issue_country =  genreDateCountryArray[l-1][genreDateCountryArray[l-1].find("(")+1:genreDateCountryArray[l-1].find(")")]
+        genre_arr_len = len(genreDateCountryArray)
+        genre = ",".join([genreDateCountryArray[x].strip() for x in range(genre_arr_len-1)])
+        issue_date = genreDateCountryArray[genre_arr_len-1].split("(")[0]
+        issue_country =  genreDateCountryArray[genre_arr_len-1][genreDateCountryArray[genre_arr_len-1].find("(")+1:genreDateCountryArray[genre_arr_len-1].find(")")]
         summary_item =  response.xpath(".//div[@class='title-overview']//div[@class='credit_summary_item']").extract()
         summary_item[0] = HtmlResponse(url=response.url, body=summary_item[0],encoding='utf-8').xpath(".//a/text()").extract()
         summary_item[2] = HtmlResponse(url=response.url, body=summary_item[2],encoding='utf-8').xpath(".//a/text()").extract()
         stars = summary_item[2][0:len(summary_item[2])-1]
         directors = str(summary_item[0])
-        
         metascore = response.xpath(".//div[@class='title-overview']//div[@class='plot_summary_wrapper']//div[@class='titleReviewBarItem']/a//span/text()").extract()
         popularity = response.xpath(".//div[@class='title-overview']//div[@class='plot_summary_wrapper']/div[@class='titleReviewBar ']/div[@class='titleReviewBarItem']/div[@class='titleReviewBarSubItem']//span[@class='subText']/text()").extract()
         popularity = "".join(re.findall('\d+',str(popularity))) 
@@ -72,9 +68,7 @@ class ImdbSpider(scrapy.Spider) :
             self.imdb["stars"] = []
             self.imdb["metascore"] = []
             self.imdb["popularity"] = []
-            
             self.initialize = True
-
 
         self.imdb["id"].append(self.items["id"][itemId])
         self.imdb["title"].append(self.items["title"][itemId])
@@ -90,33 +84,30 @@ class ImdbSpider(scrapy.Spider) :
             
         yield self.imdb
         
-    def movie_entity(self , response) :
+    def get_movie_entity(self , response) :
         
         self.imdb["id"] = response.xpath(".//tbody[@class='lister-list']/tr/td[@class='posterColumn']/span[@name='rk']/@data-value").extract()
-        
         self.imdb["title"] = response.xpath(".//tbody[@class='lister-list']/tr/td[@class='titleColumn']/a/text()").extract()
-        
         self.imdb["rating"] = response.xpath(".//tbody[@class='lister-list']/tr/td[@class='ratingColumn imdbRating']/strong/text()").extract()
-        
         self.items["id"] = self.imdb["id"]
         self.items["title"] = self.imdb["title"]
         self.items["rating"] = self.imdb["rating"]
 
     def spider_closed(self, spider):
+        logging.info("ids  " + str(self.ids))
         logging.info('Spider closed: %s', spider.name)
         data = ET.Element('entries')  
-        for i in range(249) :
+        for i in range(self.ids) :
             entry = ET.SubElement(data, 'entry')
             itemId = ET.SubElement(entry,"id") 
             for k in self.imdb.keys():
-                    
                 if k == "id" :
                     item = itemId
                 else :
                     item = ET.SubElement(entry, k)   
                 s =  self.imdb[k][i].strip()
                 item.text  = s.strip("[]").strip("\n\t\r").lstrip("\n").rstrip("\n").strip("u'\n ").strip("'")
-
+                
             mydata = minidom.parseString(ET.tostring(data)).toprettyxml()
             myfile = io.open("output.xml", "w",encoding='utf8') 
             myfile.write(mydata)  
